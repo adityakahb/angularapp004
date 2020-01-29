a96bca52f8fd95437638cb8d4ff7cd75fcce6167
abhakay <aditya.bhakay@live.com>
2020-01-29 13:07:26 +0530

    Initial setup

--- src/app/layout/siteheader/siteheader.component.ts
+++ src/app/layout/siteheader/siteheader.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 
 declare const require: any;
 declare const $: any;
-declare const Waypoint:any;
 
 let navJson = require('./../../data/megamenu.json');
 
@@ -12,36 +11,38 @@ let navJson = require('./../../data/megamenu.json');
   templateUrl: './siteheader.component.html',
   styleUrls: ['./siteheader.component.scss']
 })
-export class SiteheaderComponent implements OnInit {
+export class SiteheaderComponent implements AfterViewInit {
   navData = navJson;
   isNavOpen = false;
   isUserNavOpen = false;
   isScrolledDown = false;
   constructor() { }
 
-  ngOnInit() {
-    
+  ngAfterViewInit() {
+    $('nav:first').accessibleMegaMenu();
+    this.addScrollEvent(window);
   }
 
-  ngAfterViewInit() {
+  addScrollEvent(window: Window) {
+    let lastScrollTop = 0;
     let self = this;
-    $("nav:first").accessibleMegaMenu();
-    let waypoint = new Waypoint({
-      element: document.getElementById('siteheader-comp'),
-      handler: function(direction) {
-        if (direction === 'down') {
-          self.isScrolledDown = true;
-        } else {
-          self.isScrolledDown = false;
-        }
-      },
-      offset: '100%'
-    })
+    window.addEventListener('scroll', function() { // or window.addEventListener("scroll"....
+      let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+      if (st > lastScrollTop){
+        // downscroll code
+        self.isScrolledDown = true;
+      } else {
+          // upscroll code
+        self.isScrolledDown = false;
+      }
+      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    }, false);
   }
-  
+
   openNav() {
     this.isNavOpen = true;
   }
+
   openUserNav() {
     this.isUserNavOpen = true;
   }
